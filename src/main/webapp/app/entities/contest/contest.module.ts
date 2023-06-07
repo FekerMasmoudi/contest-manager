5a8f371caaf57f2c1ef3f5edd40f8c3a5b0b598c
FekerMasmoudi <feker.masmoudi@gmail.com>
2023-06-07 13:02:02 +0100

    Commit Front-End Contestfield

--- src/main/webapp/app/entities/contest/contest.module.ts
+++ src/main/webapp/app/entities/contest/contest.module.ts
@@ -6,8 +6,16 @@ import { ContestUpdateComponent } from './update/contest-update.component';
 import { ContestDeleteDialogComponent } from './delete/contest-delete-dialog.component';
 import { ContestRoutingModule } from './route/contest-routing.module';
 
+import { ContestfieldUpdateComponent } from '../contestfield/update/contestfield-update.component';
+
 @NgModule({
   imports: [SharedModule, ContestRoutingModule],
-  declarations: [ContestComponent, ContestDetailComponent, ContestUpdateComponent, ContestDeleteDialogComponent],
+  declarations: [
+    ContestComponent,
+    ContestDetailComponent,
+    ContestUpdateComponent,
+    ContestDeleteDialogComponent,
+    ContestfieldUpdateComponent,
+  ],
 })
 export class ContestModule {}
